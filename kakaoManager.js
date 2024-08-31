5ed31f93338ba394c1f63230e6f0a0610bceea9b
bass9030 <bass90301.naver.comm@gmail.com>
2024-08-31 21:54:10 +0900

    db controller 작성중

--- kakaoManager.js
+++ kakaoManager.js
@@ -1,3 +1,21 @@
-class kakaoManager {
-    constructor
+const mariadb = require('mariadb');
+
+const core = mariadb.createPool({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+    connectionLimit: 10
+});
+
+async function addRoom();
+async function getRooms();
+async function addMessage();
+async function getMessages();
+async function removeMessage();
+async function addMember();
+
+module.exports = {
+
 }
